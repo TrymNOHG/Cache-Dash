4a7a0762c30631716c95d223f886b8f24d74a096
TrymHG <trymhg@stud.ntnu.no>
2023-03-19 20:46:53 +0100

    feat: add data model for PictureGallery
    Create multiplicity relationship between PictureGallery and Item.

---
+++ backend/src/main/java/edu/ntnu/idatt2105/g6/backend/model/PictureGallery.java
@@ -0,0 +1,31 @@
+package edu.ntnu.idatt2105.g6.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "picture_gallery")
+public class PictureGallery {
+
+    @Id
+    @Column(name = "picture_id", nullable = false)
+    @NonNull
+    private Long pictureGalleryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    @ToString.Exclude
+    private Item item;
+
+    @Column(name = "picture", nullable = false)
+    @Lob
+    private byte @NonNull [] picture;
+
+}
