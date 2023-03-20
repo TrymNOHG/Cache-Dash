03eb2885223e28b53f39d80313411d270e399724
TrymHG <trymhg@stud.ntnu.no>
2023-03-20 17:29:19 +0100

    feat: add Password Update DTO

--- backend/src/main/java/edu/ntnu/idatt2105/g6/backend/dto/users/UserDeletionDTO.java
+++ backend/src/main/java/edu/ntnu/idatt2105/g6/backend/dto/users/UserDeletionDTO.java
@@ -3,5 +3,5 @@ package edu.ntnu.idatt2105.g6.backend.dto.users;
 import lombok.NonNull;
 
 //TODO do we token??
-public record DeleteUserDTO(@NonNull String username, @NonNull String userToDelete) {
+public record UserDeletionDTO(@NonNull String username, @NonNull String userToDelete) {
 }
