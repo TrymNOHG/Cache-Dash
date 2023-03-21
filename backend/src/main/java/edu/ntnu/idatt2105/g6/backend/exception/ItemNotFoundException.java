07f9e21e7a761c75c075be356d3b2e71680765db
TrymHG <trymhg@stud.ntnu.no>
2023-03-21 17:00:47 +0100

    feat: add deleteListing to ItemService

--- backend/src/main/java/edu/ntnu/idatt2105/g6/backend/exception/ItemNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package edu.ntnu.idatt2105.g6.backend.exception;
-
-public class ItemNotFoundException extends RuntimeException{
-
-    public ItemNotFoundException(Long itemId) {
-        super(String.format("Item with ID %d does not exist.", itemId));
-    }
-
-}
