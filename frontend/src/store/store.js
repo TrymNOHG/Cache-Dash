9a2f5278069ba19c722a025500a2aab2e5603875
eirikelv <eirik.org@gmail.com>
2023-03-21 22:08:04 +0100

    feat: add functionality for edit user information

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -6,6 +6,12 @@ export const useLoggedInStore = defineStore('alerts', {
         isLoggedIn: false,
         token: "",
         username: "",
+        fullName:"",
+        email:"",
+        dateOfBirth:"",
+        phoneNumber:"",
+        address:"",
+        zipCode:"",
     }),
 
     getters: {
@@ -20,6 +26,34 @@ export const useLoggedInStore = defineStore('alerts', {
         },
         setUsername(username){
             this.username = username;
+        },
+        setFullName(fullName){
+            this.fullName = fullName;
+        },
+        setEmail(email){
+            this.email = email;
+        },
+        setDateOfBirth(dateOfBirth){
+            this.dateOfBirth = dateOfBirth;
+        },
+        setPhoneNumber(phoneNumber){
+            this.phoneNumber =phoneNumber ;
+        },
+        setAddress(address){
+            this.address = address;
+        },
+        setZipCode(zipCode){
+            this.zipCode = zipCode;
+        },
+        setUser(fullName, userName, email, dateOfBirth, phoneNumber, address, zipCode){
+            this.fullName = fullName;
+            this.username = userName;
+            this.email = email;
+            this.dateOfBirth = dateOfBirth;
+            this.phoneNumber = phoneNumber;
+            this.address = address;
+            this.zipCode = zipCode;
         }
+
     },
 })
