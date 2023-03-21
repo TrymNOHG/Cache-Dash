cae0db1b2bb94c1438dd2d97be07e77a78e56328
TrymHG <trymhg@stud.ntnu.no>
2023-03-21 12:36:32 +0100

    feat: add updateUser method in Service

--- backend/src/main/java/edu/ntnu/idatt2105/g6/backend/service/UserService.java
+++ backend/src/main/java/edu/ntnu/idatt2105/g6/backend/service/UserService.java
@@ -1,18 +1,12 @@
 package edu.ntnu.idatt2105.g6.backend.service;
 
-import edu.ntnu.idatt2105.g6.backend.dto.users.UserDTO;
 import edu.ntnu.idatt2105.g6.backend.dto.users.UserUpdateDTO;
 import edu.ntnu.idatt2105.g6.backend.exception.UserExistsException;
-import edu.ntnu.idatt2105.g6.backend.mapper.users.UserMapper;
 import edu.ntnu.idatt2105.g6.backend.model.users.User;
 import edu.ntnu.idatt2105.g6.backend.repo.users.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -24,19 +18,24 @@ public class UserService {
     public void updateUser(UserUpdateDTO userUpdateDTO){
 
             User user = userRepository.findByUsername(userUpdateDTO.username()).orElseThrow();
+
             Optional<User> newUser = userRepository.findByUsername(userUpdateDTO.newUsername());
             if (newUser.isPresent()) throw new UserExistsException(userUpdateDTO.newUsername());
-            //TODO: NewUsername unique?
-//            user = User
-//                    .builder()
-//                    .username(userUpdateDTO.newUsername() != null ? userUpdateDTO.username() : user.getUsername())
-//                    .email()
-//                    .fullName()
-//                    .role()
-//                    .picture()
-//                    .build();
+
+            user = User
+                    .builder()
+                    .userId(user.getUserId())
+                    .username(userUpdateDTO.newUsername() != null ? userUpdateDTO.username() : user.getUsername())
+                    .fullName(userUpdateDTO.fullName() != null ? userUpdateDTO.fullName() : user.getFullName())
+                    .email(userUpdateDTO.email() != null ? userUpdateDTO.email() : user.getEmail())
+                    .birthDate(userUpdateDTO.birthDate() != null ? userUpdateDTO.birthDate() : user.getBirthDate())
+                    .phone(userUpdateDTO.phone() != null ? userUpdateDTO.phone() : user.getPhone())
+                    .picture(userUpdateDTO.picture() != null ? userUpdateDTO.picture() : user.getPicture())
+                    .role(userUpdateDTO.role() != null ? userUpdateDTO.role() : user.getRole())
+                    .build();
 
             userRepository.save(user);
+
 
     }
 }
