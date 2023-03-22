b70ecfb5db3960a6800c471375e4e23b86414600
Leonhest <leon.hesthaug@gmail.com>
2023-03-22 09:55:03 +0100

    fix: new H2 db before each test

--- backend/src/test/java/edu/ntnu/idatt2105/g6/backend/service/AuthServiceIntegrationTest.java
+++ backend/src/test/java/edu/ntnu/idatt2105/g6/backend/service/AuthServiceIntegrationTest.java
@@ -6,10 +6,12 @@ import edu.ntnu.idatt2105.g6.backend.repo.users.UserRepository;
 import edu.ntnu.idatt2105.g6.backend.security.AuthenticationResponse;
 import edu.ntnu.idatt2105.g6.backend.service.security.AuthenticationService;
 import org.junit.Test;
+import org.junit.jupiter.api.Nested;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,13 +23,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase
 public class AuthServiceIntegrationTest {
+
 
 
     @Autowired
@@ -39,7 +45,7 @@ public class AuthServiceIntegrationTest {
     @Test
     public void register_user_returns_response(){
 
-        UserDTO userDTO = new UserDTO("Test", "123", "Test Test", "test@gmail.com", Role.USER);
+        UserDTO userDTO = new UserDTO("Test", "123", "Test Test", "test@gmail.com",null,null,null, Role.USER);
         AuthenticationResponse authenticationResponse = authenticationService.register(userDTO);
         assertNotNull(authenticationResponse);
     }
@@ -47,8 +53,8 @@ public class AuthServiceIntegrationTest {
     @Test
     public void register_user_generates_unique_tokens(){
 
-        UserDTO userDTO1 = new UserDTO("Test", "123", "Test Test", "test@gmail.com", Role.USER);
-        UserDTO userDTO2 = new UserDTO("Test2", "124", "Test Test2", "test2@gmail.com", Role.USER);
+        UserDTO userDTO1 = new UserDTO("Test", "123", "Test Test", "test@gmail.com",null,null,null, Role.USER);
+        UserDTO userDTO2 = new UserDTO("Test2", "124", "Test Test2", "test2@gmail.com",null,null,null, Role.USER);
 
         AuthenticationResponse authenticationResponse1 = authenticationService.register(userDTO1);
         AuthenticationResponse authenticationResponse2 = authenticationService.register(userDTO2);
--- backend/src/test/java/edu/ntnu/idatt2105/g6/backend/service/UserServiceIntegrationTest.java
+++ backend/src/test/java/edu/ntnu/idatt2105/g6/backend/service/UserServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package edu.ntnu.idatt2105.g6.backend.service;
 
 import edu.ntnu.idatt2105.g6.backend.dto.users.UserDTO;
 import edu.ntnu.idatt2105.g6.backend.dto.users.UserUpdateDTO;
+import edu.ntnu.idatt2105.g6.backend.exception.exists.UserExistsException;
 import edu.ntnu.idatt2105.g6.backend.exception.not_found.UserNotFoundException;
 import edu.ntnu.idatt2105.g6.backend.model.users.Role;
 import edu.ntnu.idatt2105.g6.backend.model.users.User;
@@ -9,12 +10,15 @@ import edu.ntnu.idatt2105.g6.backend.repo.users.UserRepository;
 import edu.ntnu.idatt2105.g6.backend.service.security.AuthenticationService;
 import edu.ntnu.idatt2105.g6.backend.service.users.UserService;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +26,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase
 public class UserServiceIntegrationTest {
 
     @Autowired
@@ -37,7 +43,7 @@ public class UserServiceIntegrationTest {
     @Test
     public void updateUser_updates_correct_field(){
 
-        UserDTO userDTO = new UserDTO("Test", "123", "Test test", "test@gmail.com", Role.USER);
+        UserDTO userDTO = new UserDTO("Test", "123", "Test test", "test@gmail.com",null,null,null, Role.USER);
         authenticationService.register(userDTO);
 
         UserUpdateDTO userUpdateDTO = new UserUpdateDTO("Test", null, "Mr Test", null, null, null, null, null);
@@ -50,7 +56,7 @@ public class UserServiceIntegrationTest {
     @Test
     public void updateUser_does_not_update_if_only_null(){
 
-        UserDTO userDTO = new UserDTO("Test", "123", "Test test", "test@gmail.com", Role.USER);
+        UserDTO userDTO = new UserDTO("Test", "123", "Test test", "test@gmail.com",null,null,null, Role.USER);
         authenticationService.register(userDTO);
         User user1 = userRepository.findByUsername("Test").orElseThrow();
         UserUpdateDTO userUpdateDTO = new UserUpdateDTO("Test", null, null, null, null, null, null, null);
@@ -63,12 +69,29 @@ public class UserServiceIntegrationTest {
     @Test
     public void updateUser_throws_UserNotFoundException_for_non_existing_user(){
 
-        UserDTO userDTO = new UserDTO("Test", "123", "Test test", "test@gmail.com", Role.USER);
+        UserDTO userDTO = new UserDTO("Test", "123", "Test test", "test@gmail.com",null,null,null, Role.USER);
         authenticationService.register(userDTO);
 
         UserUpdateDTO userUpdateDTO = new UserUpdateDTO("Nothing here", null, null, null, null, null, null, null);
 
         assertThrows(UserNotFoundException.class, () -> {
+            userService.updateUser(userUpdateDTO);
+        });
+
+    }
+
+    @Test
+    @AutoConfigureTestDatabase
+    public void updateUser_throws_UserExistsException_for_already_existing_user(){
+
+        UserDTO userDTO1 = new UserDTO("Test", "123", "Test test", "test@gmail.com",null,null,null, Role.USER);
+        UserDTO userDTO2 = new UserDTO("Existing", "123", "Test test", "test@gmail.com",null,null,null, Role.USER);
+        authenticationService.register(userDTO1);
+        authenticationService.register(userDTO2);
+
+        UserUpdateDTO userUpdateDTO = new UserUpdateDTO("Test", "Existing", null, null, null, null, null, null);
+
+        assertThrows(UserExistsException.class, () -> {
             userService.updateUser(userUpdateDTO);
         });
 
