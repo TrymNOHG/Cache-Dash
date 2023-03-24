dac9642540a8d3d2e3b26984a44adeb5df06e180
Tomas <tomas.beranek02@gmail.com>
2023-03-24 12:03:18 +0100

    feat: filechooser now with good css and html

--- frontend/src/components/NewItemComponent.vue
+++ frontend/src/components/NewItemComponent.vue
@@ -30,14 +30,27 @@
           :options="countyStore.allCounties"
           v-model="countyStore.county.countyName"
       />
+      <label for="cityInput">{{$t('city')}}</label>
+      <BasicInput
+          id="cityInput"
+          type="text"
+          v-model="city"
+          :error="errors.city"
+          autocomplete="street-address"
+      />
       <label for="addressInput">{{$t('address')}}</label>
       <BasicInput
           id="addressInput"
           type="text"
           v-model="address"
           :error="errors.address"
-          autocomplete="address"
+          autocomplete="street-address"
       />
+      <div v-if="latitude && longitude">
+        Latitude: {{ latitude }}<br>
+        Longitude: {{ longitude }}
+      </div>
+
       <label for="keyInfoListInput">{{$t('keyInfoList')}}</label>
       <BasicInput
           id="keyInfoListInput"
@@ -57,16 +70,15 @@
           :disabled="hasErrors"
           type="submit"
           class="-fill-gradient"
+          @click="handleSubmit()"
       >
         {{$t('register')}}
       </button>
     </fieldset>
   </form>
 </template>
-
 <script>
 import BasicInput from "@/components/basicInputComponents/BasicInput.vue";
-
 import BasicTextArea from "@/components/basicInputComponents/BasicTextArea.vue";
 import {ref} from "vue";
 import {useCategoryStore, useLoggedInStore, useCountyStore, useImageStore} from "@/store/store";
@@ -83,7 +95,9 @@ export default {
   data(){
     return{
       catStore: useCategoryStore(),
-      countyStore: useCountyStore()
+      countyStore: useCountyStore(),
+      latitude: null,
+      longitude: null,
     }
   },
 
@@ -108,7 +122,9 @@ export default {
       address: yup.string()
           .required('Address is Required'),
       keyInfoList: yup.string()
-          .required('Key information Required')
+          .required('Key information Required'),
+      city: yup.string()
+          .required('City is Required')
     });
 
     const { handleSubmit, errors } = useForm({ validationSchema });
@@ -118,6 +134,8 @@ export default {
     const { value: category } = useField('category')
     const { value: address } = useField('address')
     const { value: keyInfoList } = useField('keyInfoList')
+    const { value: city } = useField('city')
+
 
 
     const submit = handleSubmit(async () => {
@@ -138,9 +156,6 @@ export default {
       }
       formData.append('keyInfoList', keyInfoList.value.split(" "))
 
-      console.log('formdatan:' + formData.getAll('image'))
-
-
       imageStore.$reset();
     });
 
@@ -148,6 +163,7 @@ export default {
       price,
       brief,
       full,
+      city,
       address,
       errors,
       category,
@@ -168,7 +184,8 @@ export default {
         brief: this.brief,
         full: this.full,
         keyInfoList: this.keyInfoList,
-        address: this.address
+        address: this.address,
+        city: this.city
       });
     },
   },
@@ -178,18 +195,28 @@ export default {
       this.imageStore.addImage(theNewImage)
       console.log("me", this.imageStore.test)
     },
-  }
+
+    findAddressByLatLng() {
+      const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(this.address)}.json?access_token=pk.eyJ1IjoidG9tYWJlciIsImEiOiJjbGZsYmw0Ym0wMDNqM3BvMXNlZ213bXlvIn0.XAO9MuoT6FoiYXnbznnJqg`;
+      fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            this.latitude = data.features[0].center[1];
+            this.longitude = data.features[0].center[0];
+          })
+          .catch(error => console.error(error));
+    }
+  },
 }
 </script>
 
 <style scoped>
-
 * {
   box-sizing: border-box;
 }
 
 form {
-  padding: 20px 0;
+  padding: 20px;
   grid-column: 2;
   background-color: #7EB09B;
 }
@@ -201,6 +228,7 @@ textarea {
   border: 2px solid #ccc;
   margin-bottom: 10px;
   font-size: 1rem;
+  width: 100%;
 }
 
 h1,
@@ -215,17 +243,19 @@ h1 {
 }
 
 label{
+  display: block;
   padding: 10px;
 }
 
 /* Common styles for button elements */
 button {
-  border-width: 2px;
-  border-color: black;
+  border: 2px solid black;
   padding: 10px 20px;
   background-color: #FFD700;
   color: black;
-  margin: 10px;
+  margin: 10px 5px;
+  border-radius: 5px;
+  cursor: pointer;
 }
 
 button:active {
@@ -234,13 +264,16 @@ button:active {
 }
 
 button:hover {
-  color: #1E293B !important;
-  background-color: white !important;
+  color: #1E293B;
+  background-color: white;
+  border-color: #1E293B;
 }
 
-button:disabled{
-  background-color: black;
+button:disabled {
+  background-color: #b3a100;
+  cursor: not-allowed;
 }
+
 /* Style for error messages */
 h5 {
   color: red;
@@ -255,6 +288,4 @@ fieldset {
   display: flex;
   flex-direction: column;
 }
-
-
 </style>
