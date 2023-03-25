1764f39806222ae8c523ba2663a91a2e6e5fd85f
Leonhest <leon.hesthaug@gmail.com>
2023-03-25 21:11:39 +0100

    docs: add Javadoc for the repository package

--- backend/src/main/java/edu/ntnu/idatt2105/g6/backend/repo/chat/ConversationRepository.java
+++ backend/src/main/java/edu/ntnu/idatt2105/g6/backend/repo/chat/ConversationRepository.java
@@ -8,10 +8,34 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+
+ This repository provides CRUD operations for the Conversation entity.
+
+ It extends JpaRepository and JpaSpecificationExecutor interfaces.
+
+ JpaRepository provides basic CRUD operations while JpaSpecificationExecutor provides
+ search functionality using specifications.
+ */
 @Repository
 public interface ConversationRepository extends JpaRepository<Conversation, Long> {
+
+    /**
+     * Finds a conversation by its ID.
+     *
+     * @param conversationId The ID of the conversation to find.
+     * @return An Optional containing the conversation, or an empty Optional if the conversation was not found.
+     */
     Optional<Conversation> findByConversationId(Long conversationId);
 
+    /**
+     * Finds all conversations that involve a given user.
+     *
+     * @param user One of the users involved in the conversations.
+     * @param user2 The other user involved in the conversations.
+     * @return An Optional containing a List of all conversations involving the given user,
+     *         or an empty Optional if no conversations were found.
+     */
     Optional<List<Conversation>> findAllByUser1OrUser2(User user, User user2);
 
 }
