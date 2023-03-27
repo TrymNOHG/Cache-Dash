65b1cf2b7723aae1a6554c767b37885fd29ddf19
eirikelv <eirik.org@gmail.com>
2023-03-27 23:46:38 +0200

    refactor: remove speachbuble

--- frontend/src/components/pagesComponents/ItemPage/SellerInformationComponent.vue
+++ frontend/src/components/pagesComponents/ItemPage/SellerInformationComponent.vue
@@ -63,36 +63,4 @@ export default {
   .chat-button{
     height: inherit;
   }
-
-
-  .speech-bubble {
-    position: relative;
-    margin: .5em auto;
-    padding: 1em;
-    width: 10em; height: 4em;
-    border-radius: .25em;
-    transform: rotate(-4deg) rotateY(15deg);
-    background: #000000;
-    font: 2em/4 sans-serif;
-    text-align: center;
-  }
-  .speech-bubble:before, .speech-bubble:after {
-    position: absolute;
-    z-index: -1;
-    content: '';
-  }
-  .speech-bubble:after {
-    top: 0; right: 0; bottom: 0; left: 0;
-    border-radius: inherit;
-    transform: rotate(2deg) translate(.35em, -.15em) scale(1.02);
-    background: #ffd700;
-  }
-  .speech-bubble:before {
-    border: solid 0 transparent;
-    border-right: solid 3.5em #ffd700;
-    border-bottom: solid .25em #000000;
-    bottom: .25em; left: 1.25em;
-    width: 0; height: 1em;
-    transform: rotate(45deg) skewX(75deg);
-  }
 </style>
